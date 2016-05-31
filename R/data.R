819d64df4c67cc2742ed9b3bf01c38ca60598fef
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-05-31 17:07:11 +1200

    document example data

---
+++ R/data.R
@@ -0,0 +1,51 @@
+#' Example data for heatmap demonstration
+#'
+#' A randomly generated numeric matrix for heatmap con continous scale
+#'
+#' @format A matrix with 50 rows and 20 columns:
+"mat"
+
+#' A default number of breaks to use
+#'
+#' @format 50:
+"n"
+
+#' A colorbar example for columns
+#'
+#' @format A vector for colors of 20 columns:
+"colbar"
+
+#' A colorbar example for rows
+#'
+#' @format A vector for colors of 50 rows:
+"rowbar"
+
+#' A colorbar example for columns
+#'
+#' @format A vector for colors of 20 columns:
+"colbars1"
+
+#' A colorbar example for columns
+#'
+#' @format A vector for colors of 20 columns:
+"colbars2"
+
+#' A colorbar example for rows
+#'
+#' @format A vector for colors of 50 rows:
+"rowbars1"
+
+#' A colorbar example for rows
+#'
+#' @format A vector for colors of 50 rows:
+"rowbars2"
+
+#' Example color annotation for columns heatmap demonstration
+#'
+#' @format A character matrix with 2 color rows and 20 columns:
+"colbarmat"
+
+#' Example color annotation for rows heatmap demonstration
+#'
+#' @format A character matrix with 50 rows and 2 color columns:
+"rowbarmat"
